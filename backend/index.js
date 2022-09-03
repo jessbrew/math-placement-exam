b6dfb1830d6d30e36138c4da52a1ad1ee338096e
John Karow <johnkarow@Johns-MacBook-Pro.local>
2022-09-03 17:29:04 -0500

    Added a swagger page

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,26 @@
 const express = require("express")
 const sql = require('mssql')
 const dotenv = require('dotenv')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+// const routes = require("../backend/routes")
 var cors = require('cors');
 dotenv.config()
-var app = express()
-
+var app = express(), bodyParser = require("body-parser")
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 //Routes 
 const health = require("./routes/health.js")
 const answers = require("./routes/answers.js")
+const req = require("express/lib/request")
 
 //Specifying ports
 PORT = 3000
+app.use(bodyParser.json());
 app.use("/", health)
 app.use("/", answers)
 
 app.listen((PORT), () => {
     console.log("Server is running on port:" + PORT)
 })
+
+// require('./routes')(app)
