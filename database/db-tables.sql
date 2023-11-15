db0f7b5563165a56f91ec5c2b0cdf26cefb939ff
outi5 <45103903+outi5@users.noreply.github.com>
2023-11-14 19:41:15 -0600

    update sql files for postgresql (first attempt)

--- database/db-tables.sql
+++ database/db-tables.sql
@@ -1,178 +1,101 @@
--- Create tests
-CREATE TABLE [dbo].[tests](
-	[test_id] [int] NOT NULL,
-	[test_name] [varchar](100) NULL,
-	[time_limit] [int] NULL,
- CONSTRAINT [PK_tests_test_id] PRIMARY KEY CLUSTERED 
+-- Create Table: tests
+CREATE TABLE IF NOT EXISTS dbo.tests
 (
-	[test_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+    test_id serial NOT NULL PRIMARY KEY,
+    test_name varchar(100) NULL,
+    time_limit int NULL
+);
 
--- Create question_types
-CREATE TABLE [dbo].[question_types](
-	[question_type_id] [int] IDENTITY(1,1) NOT NULL,
-	[type_title] [varchar](200) NULL,
- CONSTRAINT [PK_question_types_question_type_id] PRIMARY KEY CLUSTERED 
+-----------------------------------------------------------
+-- Create Table: question_types
+CREATE TABLE IF NOT EXISTS dbo.question_types
 (
-	[question_type_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+    question_type_id serial NOT NULL PRIMARY KEY,
+    type_title varchar(200) NULL
+);
 
--- Create questions
-CREATE TABLE [dbo].[questions](
-	[question_id] [int] IDENTITY(1,1) NOT NULL,
-	[question_text] [varchar](max) NULL,
-	[question_type] [int] NULL,
- CONSTRAINT [PK_questions_question_id] PRIMARY KEY CLUSTERED 
+-----------------------------------------------------------
+-- Create Table: questions
+CREATE TABLE IF NOT EXISTS dbo.questions
 (
-	[question_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
+    question_id serial NOT NULL PRIMARY KEY,
+    question_text varchar NULL,
+    question_type int NULL
+);
 
--- Create test_questions
-CREATE TABLE [dbo].[test_questions](
-	[test_question_id] [int] IDENTITY(1,1) NOT NULL,
-	[question_id] [int] NOT NULL,
-	[test_id] [int] NOT NULL,
- CONSTRAINT [PK_test_question_id] PRIMARY KEY CLUSTERED 
+-----------------------------------------------------------
+-- Create Table: test_questions
+CREATE TABLE IF NOT EXISTS dbo.test_questions
 (
-	[test_question_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+    test_question_id serial NOT NULL PRIMARY KEY,
+    question_id int NOT NULL,
+    test_id int NOT NULL,
+    CONSTRAINT fk_test_questions_question_id FOREIGN KEY(question_id) REFERENCES dbo.questions(question_id),
+    CONSTRAINT fk_test_questions_test_id FOREIGN KEY(test_id) REFERENCES dbo.tests(test_id)
+);
 
-ALTER TABLE [dbo].[test_questions]  WITH CHECK ADD  CONSTRAINT [FK_test_questions_question_id] FOREIGN KEY([question_id])
-REFERENCES [dbo].[questions] ([question_id])
-GO
-
-ALTER TABLE [dbo].[test_questions] CHECK CONSTRAINT [FK_test_questions_question_id]
-GO
-
-ALTER TABLE [dbo].[test_questions]  WITH CHECK ADD  CONSTRAINT [FK_test_questions_test_id] FOREIGN KEY([test_id])
-REFERENCES [dbo].[tests] ([test_id])
-GO
-
-ALTER TABLE [dbo].[test_questions] CHECK CONSTRAINT [FK_test_questions_test_id]
-GO
-
-
--- Create answers
-CREATE TABLE [dbo].[answers](
-	[answer_id] [int] IDENTITY(1,1) NOT NULL,
-	[question_id] [int] NOT NULL,
-	[answer_text] [varchar](max) NULL,
-	[is_correct] [bit] NULL,
- CONSTRAINT [PK_answers_answer_id] PRIMARY KEY CLUSTERED 
+-----------------------------------------------------------
+-- Create Table: answers
+CREATE TABLE IF NOT EXISTS dbo.answers
 (
-	[answer_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
+    answer_id serial NOT NULL PRIMARY KEY,
+    question_id int NOT NULL,
+    answer_text varchar NULL,
+    is_correct boolean NULL,
+    CONSTRAINT fk_answers_question_id FOREIGN KEY(question_id) REFERENCES dbo.questions(question_id)
+);
 
-ALTER TABLE [dbo].[answers]  WITH CHECK ADD  CONSTRAINT [FK_answers_question_id] FOREIGN KEY([question_id])
-REFERENCES [dbo].[questions] ([question_id])
-GO
-
-ALTER TABLE [dbo].[answers] CHECK CONSTRAINT [FK_answers_question_id]
-GO
-
--- Create past_courses
-CREATE TABLE [dbo].[past_courses](
-	[past_course_id] [int] IDENTITY(1,1) NOT NULL,
-	[display_order] [int] NULL,
-	[description] [varchar](300) NULL,
-	[test_type] [varchar](100) NULL,
-
-PRIMARY KEY CLUSTERED 
+-----------------------------------------------------------
+-- Create Table: past_courses
+CREATE TABLE IF NOT EXISTS dbo.past_courses
 (
-	[past_course_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+    past_course_id serial NOT NULL PRIMARY KEY,
+    display_order int NULL,
+    description varchar(300) NULL,
+    test_type varchar(100) NULL
+);
 
--- Create students
-CREATE TABLE [dbo].[students](
-	[student_id] [bigint] IDENTITY(1,1) NOT NULL,
-	[wlc_id] [bigint] NULL,
-	[first_name] [varchar](200) NULL,
-	[last_name] [varchar](200) NULL,
-	[advisor] [varchar](200) NULL,
-	[most_advanced_class_taken] [varchar](200) NULL,
-	[most_advanced_class_grade] [varchar](10) NULL,
-	[desired_class] [varchar](100) NULL,
-	[math_in_last_year] [bit] NULL,
-	[student_guid][varchar](200) NULL,
-	[test_id] [int] NULL,
-	[question_id] [int] NULL,
-	[inserted_on] [datetime] NULL,
-	[test_completed] [bit] NULL,
-	[is_active] [bit] NULL,
-	[start_time] [datetime] NULL,
-PRIMARY KEY CLUSTERED 
+-----------------------------------------------------------
+-- Create Table: students
+CREATE TABLE IF NOT EXISTS dbo.students
 (
-	[student_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+    student_id bigserial NOT NULL PRIMARY KEY,
+    math_in_last_year bit NULL,
+    student_guid varchar(200) NULL,
+    test_id int NULL,
+    inserted_on timestamp(3) without time zone NULL,
+    test_completed bit NULL,
+    start_time timestamp(3) without time zone NULL,
+    -- CONSTRAINT df_inserted  DEFAULT (getdate()) FOR inserted_on,
+    -- CONSTRAINT df_completed  DEFAULT ((0)) FOR test_completed,
+    CONSTRAINT fk_tests_test_id FOREIGN KEY(test_id) REFERENCES dbo.tests (test_id)
+);
+			-- ALTER TABLE [dbo].[students] ADD  CONSTRAINT [df_inserted]  DEFAULT (getdate()) FOR [inserted_on]
+			-- GO
 
-ALTER TABLE [dbo].[students] ADD  CONSTRAINT [df_inserted]  DEFAULT (getdate()) FOR [inserted_on]
-GO
+			-- ALTER TABLE [dbo].[students] ADD  CONSTRAINT [df_completed]  DEFAULT ((0)) FOR [test_completed]
+			-- GO
 
-ALTER TABLE [dbo].[students] ADD  CONSTRAINT [df_completed]  DEFAULT ((0)) FOR [test_completed]
-GO
-
-ALTER TABLE [dbo].[students] ADD  DEFAULT ((1)) FOR [is_active]
-GO
-
-ALTER TABLE [dbo].[students]  WITH CHECK ADD FOREIGN KEY([question_id])
-REFERENCES [dbo].[questions] ([question_id])
-GO
-
-ALTER TABLE [dbo].[students]  WITH CHECK ADD FOREIGN KEY([test_id])
-REFERENCES [dbo].[tests] ([test_id])
-GO
-
--- Create student_past_courses
-CREATE TABLE [dbo].[student_past_courses](
-	[student_past_course_id] [int] IDENTITY(1,1) NOT NULL,
-	[student_id] [bigint] NULL,
-	[past_course_id] [int] NULL,
-PRIMARY KEY CLUSTERED 
+-----------------------------------------------------------
+-- Create Table: student_past_courses
+CREATE TABLE IF NOT EXISTS dbo.student_past_courses
 (
-	[student_past_course_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+    student_past_course_id serial NOT NULL PRIMARY KEY,
+    student_id bigint NULL,
+    course_grade varchar(10),
+    past_course_id int NULL,
+    CONSTRAINT fk_past_courses_past_course_id FOREIGN KEY(past_course_id) REFERENCES dbo.past_courses (past_course_id),
+    CONSTRAINT fk_student_student_id FOREIGN KEY(student_id) REFERENCES dbo.students (student_id)
+);
 
-ALTER TABLE [dbo].[student_past_courses]  WITH CHECK ADD FOREIGN KEY([past_course_id])
-REFERENCES [dbo].[past_courses] ([past_course_id])
-GO
-
-ALTER TABLE [dbo].[student_past_courses]  WITH CHECK ADD FOREIGN KEY([student_id])
-REFERENCES [dbo].[students] ([student_id])
-GO
-
--- student_answers
-CREATE TABLE [dbo].[student_answers](
-	[student_answer_id] [bigint] IDENTITY(1,1) NOT NULL,
-	[student_id] [bigint] NULL,
-	[answer_id] [int] NULL,
-	[time_submitted] [datetime] NULL
-PRIMARY KEY CLUSTERED 
+-----------------------------------------------------------
+-- Create Table: student_answers
+CREATE TABLE IF NOT EXISTS dbo.student_answers
 (
-	[student_answer_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[student_answers]  WITH CHECK ADD FOREIGN KEY([answer_id])
-REFERENCES [dbo].[answers] ([answer_id])
-GO
-
-ALTER TABLE [dbo].[student_answers]  WITH CHECK ADD FOREIGN KEY([student_id])
-REFERENCES [dbo].[students] ([student_id])
-GO
+    student_answer_id bigserial NOT NULL PRIMARY KEY,
+    student_id bigint NULL,
+    answer_id int NULL,
+    time_submitted timestamp(3) without time zone NULL,
+    CONSTRAINT fk_answers_answer_id FOREIGN KEY(answer_id) REFERENCES dbo.answers (answer_id),
+    CONSTRAINT fk_students_student_id FOREIGN KEY(student_id) REFERENCES dbo.students (student_id)
+);
