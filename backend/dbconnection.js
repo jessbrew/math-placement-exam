0e9a8ab52c4e4ca70ca975e484ceeea5979b3861
outi5 <45103903+outi5@users.noreply.github.com>
2024-07-01 08:58:23 -0500

    updated DB connection to use postgres

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -1,19 +1,27 @@
-const sql = require("pg-pool");
+const pool = require("pg-pool");
 
 const dbConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.DB_HOST,
-    database: process.env.DB_Database,
-    stream: false,
-    options: {
-        trustedConnection: true,
-        encrypt: true,
-        enableArithAbort: true,
-        trustServerCertificate: true,
-    },
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    // stream: false,
+    // ssl: true
+    // options: {
+    //     trustedConnection: true,
+    //     encrypt: true,
+    //     enableArithAbort: true,
+    //     trustServerCertificate: true,
+    // },
 };
 
-let dbConn = new sql(dbConfig);
+
+let dbConn = new pool(dbConfig);
+
+dbConn.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1)
+  })
 
 module.exports = dbConn;
