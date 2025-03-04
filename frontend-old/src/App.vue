73145e0eb51c4a4a4ea99a3f0c2f4f5661d59949
Jess Brewster <jessbrewster@Jesss-MacBook-Pro.local>
2025-03-04 12:57:08 -0600

    Updating to vue3, getting home page

---
+++ frontend-old/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <math-test-header></math-test-header>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import MathTestHeader from "@/components/general/MathTestHeader.vue";
+export default {
+  name: "App",
+  components: {
+    MathTestHeader,
+  },
+  // beforeMount() {
+  //   // Does not work without user interaction
+  //   window.addEventListener("beforeunload", function (event) {
+  //     console.log("User tried to leave the page."); // Replace with something else
+  //     event.returnValue = "Are you sure you want to leave the test?"; // Older browsers apparently show this on prompt
+  //     event.preventDefault();
+  //   });
+  // },
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,69 @@
+<script setup>
+</script>
+
+<template>
+    <div class="instructions">
+    <h1>General Instructions</h1>
+    <ul>
+      <li>
+        <span class="important"
+          >IMPORTANT: Please read all instructions before starting the
+          test!</span
+        >
+      </li>
+      <li>
+        <span class="important"
+          >You may use one piece of scrap paper to work out answers.</span
+        >
+      </li>
+      <li>
+        <span class="important"
+          >NO calculators, NO books, NO notes are allowed while taking the
+          test.</span
+        >
+      </li>
+      <li>Once you start the test, you must finish it in one sitting.</li>
+      <li>
+        You will be presented one question at a time. Once you've answered it,
+        you cannot change your answer or go back.
+      </li>
+      <li>
+        When you have 10 minutes left for the test you will receive a reminder.
+      </li>
+      <li>If you run out of time, the test will end.</li>
+      <li>
+        You can only log in once. If you lose your connection, you will have to take the entire exam again.
+      </li>
+      <li>
+        If you have any computer problems or questions, please call the WLC
+        Technology Help Desk at (414) 443-8911 for assistance.
+      </li>
+      <li>
+        When you are ready, click "Begin Test" to go to the log-in screen.
+      </li>
+      <li>
+        You will need your WLC ID number to login. Contact the registrar's
+        office (email:
+        <a href="mailto:registrarsoffice@wlc.edu">registrarsoffice@wlc.edu</a>
+        or phone: <a href="tel:+14144438785"> (414) 443-8785)</a> if you need
+        your number.
+      </li>
+    </ul>
+    </div>
+</template>
+
+<style>
+.important {
+  color: rgb(119, 36, 50);
+  font-weight: bold;
+}
+.instructions a {
+  color: rgb(0, 139, 149);
+}
+.instructions h1 {
+    padding-top: 10px;
+}
+.instructions ul {
+    padding-left: 20px;
+}
+</style>
